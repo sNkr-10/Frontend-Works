544f5a2379019e40f2e086ae68c285529ad9f3b3
gwrisnkr <gowrisankar@neoito.com>
2021-06-11 15:57:07 +0530

    committed

--- Workshop_2/js/script.js
+++ Workshop_2/js/script.js
@@ -7,3 +7,71 @@ const setActive = (event)=> {
           event.target.className = "nav-link active"
      }
 }
+
+const setHover = ()=> {
+     let elements = document.getElementsByClassName("item-child")
+     if (event.target.className != "shadow-lg bg-white") {
+          for (var i=0;i<elements.length;i++) {
+               elements[i].classList.remove('shadow-lg')
+               elements[i].classList.remove('bg-white')
+               elements[i].classList.remove('active')
+          } 
+          event.target.classList.add("active")
+          event.target.classList.add('shadow-lg')
+          event.target.classList.add('bg-white')
+     }
+}
+
+$('.slick-first').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
+
+$('.slick-second').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
