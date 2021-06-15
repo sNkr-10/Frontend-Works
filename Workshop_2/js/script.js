78820a19767adcb5c6f9f4d38881d074c83dde98
gwrisnkr <gowrisankar@neoito.com>
2021-06-15 21:38:51 +0530

    Finished with remaining some minor bugs

--- Workshop_2/js/script.js
+++ Workshop_2/js/script.js
@@ -1,25 +1,55 @@
 const setActive = (event)=> {
-     let elements = document.getElementsByClassName("nav-link")
-     if (event.target.className != "nav-link active") {
-          for (var i=0;i<elements.length;i++) {
-               elements[i].classList.remove('active')
-          } 
-          event.target.className = "nav-link active"
-     }
+    let elements = document.getElementsByClassName("nav-link")
+    if (event.target.className != "nav-link active") {
+      for (var i=0;i<elements.length;i++) {
+           elements[i].classList.remove('active')
+      } 
+      event.target.classList.add("active")
+    }
+}
+
+const showMenu = ()=> { 
+    let menu = document.querySelector(".side-menu")
+    let button = document.querySelector("#spotlight header button")
+    menu.classList.toggle("active")
+    button.classList.toggle("active")
+}
+
+const removeMenu = ()=> { 
+    let menu = document.querySelector(".side-menu")
+    let button = document.querySelector("#spotlight header button")
+    menu.classList.remove("active")
+    button.classList.remove("active")
+
 }
 
 const setHover = ()=> {
-     let elements = document.getElementsByClassName("item-child")
-     if (event.target.className != "shadow-lg bg-white") {
-          for (var i=0;i<elements.length;i++) {
-               elements[i].classList.remove('shadow-lg')
-               elements[i].classList.remove('bg-white')
-               elements[i].classList.remove('active')
-          } 
-          event.target.classList.add("active")
-          event.target.classList.add('shadow-lg')
-          event.target.classList.add('bg-white')
-     }
+    let elements = document.getElementsByClassName("item-child")
+    if (event.target.className != "shadow-lg bg-white") {
+        for (var i=0;i<elements.length;i++) {
+             elements[i].classList.remove('shadow-lg')
+             elements[i].classList.remove('bg-white')
+             elements[i].classList.remove('active')
+        } 
+        event.target.classList.add("active")
+        event.target.classList.add('shadow-lg')
+        event.target.classList.add('bg-white')
+    }
+}
+
+
+const setDropdown = (event)=> {
+    let elements = document.getElementsByClassName("item-child")
+    let arrow = document.querySelector("#services .middle .item .item-child i")
+    if (event.target.className != "item-child p-xl-4 p-lg-3 p-sm-2 rounded d-flex flex-column justify-content-between align-items-center drop") {
+        for (var i=0;i<elements.length;i++) {
+             elements[i].classList.remove('drop')
+        }
+        event.target.classList.add("drop")
+    }
+    else {
+        event.target.classList.remove("drop")
+    }
 }
 
 $('.slick-first').slick({
@@ -46,6 +76,7 @@ $('.slick-first').slick({
   ]
 });
 
+
 $('.slick-second').slick({
   dots: true,
   infinite: true,
@@ -66,12 +97,54 @@ $('.slick-second').slick({
       settings: {
         slidesToShow: 2,
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      }
     }
   ]
 });
+
+$('.slick-third').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 9999,
+        settings: "unslick"
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+});
+
+ function form_validation() {
+        var name = document.forms["RegForm"]["Name"];
+        var email = document.forms["RegForm"]["EMail"];
+  
+        if (name.value == "") {
+            window.alert("Please enter your name.");
+            name.focus();
+            return false;
+        }
+  
+        if (email.value == "") {
+            window.alert(
+              "Please enter a valid e-mail address.");
+            email.focus();
+            return false;
+        }
+  
+        return true;
+    }
